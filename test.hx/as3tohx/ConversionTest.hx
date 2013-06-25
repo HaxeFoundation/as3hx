3c5c3da928e89bff0d249d6d961b90d2ab760f8d
Yannick <yannick.dominguez@gmail.com>
2013-06-25 23:23:25 +0200

    updated generated file

--- test.hx/as3tohx/ConversionTest.hx
+++ test.hx/as3tohx/ConversionTest.hx
@@ -21,44 +21,104 @@
 package as3tohx;
 
 // Additional implicit imports...
+import as3tohx.AnotherClass;
+import as3tohx.ISomeInterface;
+import as3tohx.KeyClass;
 import as3tohx.MyClass;
 import as3tohx.UInt;
+import as3tohx.ValueClass;
+import haxe.ds.IntMap;
+import haxe.ds.StringMap;
 
 /**
  *  This class is marked with final and
  *  should be converted to the Haxe "@:final"
  */
-class @:final Main extends MyClass
+class @:final Main extends MyClass implements ISomeInterface
 {
+    public var intKeyMap(get_intKeyMap, never) : IntMap<ValueClass>;
+    public var stringKeyMap(get_stringKeyMap, never) : StringMap<ValueClass>;
+    public var objectKeyMap(get_objectKeyMap, never) : Map<KeyClass, ValueClass>;
+    public var mapOfArray(get_mapOfArray, never) : IntMap<Array<AnotherClass>>;
+    public var mapOfMap(get_mapOfMap, never) : IntMap<StringMap<AnotherClass>>;
     public var sampleProperty(get_sampleProperty, set_sampleProperty) : Dynamic;
+
+    static public var someMonths : Array<Dynamic> = ["January", "February", "March"];
+    static public var someDay : Array<Dynamic> = ["January", 1, 1970, "AD"];
     
-    var isResult1 : Bool = true;    
-    var isResult2 : Bool = true;    
-    var isResult3 : Bool = true;    
-    var isResult4 : Bool = true;    
+    var mIntKeyMap : IntMap<ValueClass> = null;
+    var mStringKeyMap : StringMap<ValueClass> = null;
+    var mObjectKeyMap : Map<KeyClass, ValueClass> = null;
+    var mMapOfArray : IntMap<Array<AnotherClass>> = null;
+    var mMapOfMap : IntMap<StringMap<AnotherClass>> = null;
     
-    // the line below is to test assigning a value    
-    // during variable declaration    
-    var intVal : Int = 6;    
+    @:final function get_intKeyMap() : IntMap<ValueClass>
+    {
+        if (mIntKeyMap == null){
+            mIntKeyMap = new IntMap<ValueClass>();
+        }
+        return mIntKeyMap;
+    }
     
-    var _sampleProperty : Dynamic;    
+    @:final function get_stringKeyMap() : StringMap<ValueClass>
+    {
+        if (mStringKeyMap == null){
+            mStringKeyMap = new StringMap<ValueClass>();
+        }
+        return mStringKeyMap;
+    }
+    
+    @:final function get_objectKeyMap() : Map<KeyClass, ValueClass>
+    {
+        if (mObjectKeyMap == null){
+            mObjectKeyMap = new Map<KeyClass, ValueClass>();
+        }
+        return mObjectKeyMap;
+    }
+    
+    @:final function get_mapOfArray() : IntMap<Array<AnotherClass>>
+    {
+        if (mMapOfArray == null){
+            mMapOfArray = new IntMap<Array<AnotherClass>>();
+        }
+        return mMapOfArray;
+    }
+    
+    @:final function get_mapOfMap() : IntMap<StringMap<AnotherClass>>
+    {
+        if (mMapOfMap == null){
+            mMapOfMap = new IntMap<StringMap<AnotherClass>>();
+        }
+        return mMapOfMap;
+    }
+    
+    var isResult1 : Bool = true;
+    var isResult2 : Bool = true;
+    var isResult3 : Bool = true;
+    var isResult4 : Bool = true;
+    
+    // the line below is to test assigning a value
+    // during variable declaration
+    var intVal : Int = 6;
+    
+    var _sampleProperty : Dynamic;
     
     /**
      * GETTER & SETTER STATEMENTS
      * This section tests to make property set / get methods private
      * and make them named with set_ get_
-     */    
+     */
     
     function get_sampleProperty() : Dynamic
     {
         return _sampleProperty;
-    }    
+    }
     
     function set_sampleProperty(value : Dynamic) : Dynamic
     {
         _sampleProperty = value;
         return value;
-    }    
+    }
     
     public function testPublicMethod() : Void
     {
@@ -87,9 +147,9 @@ class @:final Main extends MyClass
         // seperate line with no comments but
         // with white space
         //
-        funcB(paramA, 
-                paramB, 
-                paramC, 
+        funcB(paramA,
+                paramB,
+                paramC,
                 paramD);
         
         /**
@@ -97,10 +157,10 @@ class @:final Main extends MyClass
          * seperate line with comments and
          * white space to preserve
          */
-        funcC(paramA, // comment on paramA
-                paramB, /* comment describing paramB */
-                paramC, 
-                paramD);// one more comment
+        funcC(paramA,  // comment on paramA  
+                paramB,  /* comment describing paramB */  
+                paramC,
+                paramD);  // one more comment  
         
         
         /**
@@ -108,8 +168,8 @@ class @:final Main extends MyClass
          * lines, with comments interspersed and
          * white space
          */
-        retValue = funcD(paramA, // comment on paramA
-                        paramB, paramC, /* comment describing paramB */
+        retValue = funcD(paramA,  // comment on paramA  
+                        paramB, paramC,  /* comment describing paramB */  
                         paramD);
         
         
@@ -119,9 +179,9 @@ class @:final Main extends MyClass
          * multiple lines, comments interspersed, and
          * white space to preserve
          */
-        retValue = funcE(valueA + valueB + valueC + // comment on paramA, B, C
-                        valueD + valueE + valueF, // comment on paramD, E, F
-                        paramA, paramB, /* comment */paramC, /* comment describing paramC */
+        retValue = funcE(valueA + valueB + valueC +  // comment on paramA, B, C  
+                        valueD + valueE + valueF,  // comment on paramD, E, F  
+                        paramA, paramB,  /* comment */  paramC,  /* comment describing paramC */  
                         // last comment
                         paramD);
         
@@ -136,7 +196,7 @@ class @:final Main extends MyClass
          * If statement:
          * simple if different style
          */
-        if (isResult1) 
+        if (isResult1)
         // simple if with comments
         {
             isResult1 = true;
@@ -147,36 +207,36 @@ class @:final Main extends MyClass
          * if statement:
          * series of else if statements with comments
          */
-        if (isResult1) 
+        if (isResult1)
         {
             // comment within if
             isResult1 = true;
         }
         // comment line
-        else if (isResult2) 
+        else if (isResult2)
         {
             trace("trace line");
             
             // Testing nested else/if statements
             
-            if (isResult3) 
+            if (isResult3)
             {
                 // Testing that "map.hasOwnProperty(xxx)" is
                 // replaced with "map.exists(xxx)"
-                value = map.hasOwnProperty(myClass);
+                value = map.exists(myClass);
             }
-            else if (isResult4) 
+            else if (isResult4)
             {
                 // Testing that "hasAnyProperties(map)" is
                 // replaced with "map.keys().hasNext()"
-                value = hasAnyProperties(map);
+                value = map.keys().hasNext();
             }
         }
         /* comment line */
-        else if (isResult3) // coment at the end of the line
+        else if (isResult3)  // coment at the end of the line  
         {
             // one nested if
-            if (!isResult1) 
+            if (!isResult1)
                 trace("trace line");
         }
         
@@ -188,36 +248,37 @@ class @:final Main extends MyClass
         var value : Int;
         
         switch (param)
-        {            
+        {
             case 0:
             {
                 value = 1;
-            }            
-            // comment line            
-            case funcT(param):// comment explaining function
+            }
+            // comment line
+            case funcT(param):  // comment explaining function  
             {
                 // The if statement below tests that
                 // 'if (obj)' is converted into 'if (obj != null)'
                 
-                if (obj) 
+                var obj : Dynamic = { };
+                if (obj != null)
                 {
                     trace("trace line");
                 }
                 
                 value = 2;
-            }            
+            }
             
             /*
              Another comment line
              which usually disappear when
              the break is removed and leaves
              the dangling semicolon
-             */            
+             */
             case 1:
             {
-                value = 1;/* comment line1
+                value = 1;  /* comment line1
                    comment line2
-                   comment line3 */
+                   comment line3 */  
                 value = 2;
                 /*
                 // the lines below tests if the converter
@@ -226,15 +287,15 @@ class @:final Main extends MyClass
                 // The converter needs to preserve line breaks
                 */
                 
-                value = "string part1" + "string part2" + 
+                value = "string part1" + "string part2" +
                         "string part3" + "string part4";
-            }            
+            }
             
             default:
                 value = 0;
         }
         return true;
-    }    
+    }
     
     /**
     * FUNCTION DEFINITION FORMATS
@@ -246,25 +307,25 @@ class @:final Main extends MyClass
     * with comments and white space to preserve
     * Also this function is marked as "final" and
     * should be converted to the Haxe "@:final"
-    */    
-    public @:final function testPublicMethod3(var1 : Bool, // comment line 1
-            var2 : String,var3 : UInt, /* comment line 2 */
-            func4 : Dynamic,var5 : Array<Dynamic>) : Bool // comment line 3
+    */
+    public @:final function testPublicMethod3(var1 : Bool,  // comment line 1  
+            var2 : String,var3 : UInt,  /* comment line 2 */  
+            func4 : Dynamic,var5 : Array<Dynamic>) : Bool  // comment line 3  
     {
         return true;
-    }    
+    }
     
     /**
     * All function arguments are on individual lines
     * with comments inbetween
-    */    
+    */
     public function testPublicMethod5(var1 : Bool,
             var2 : String,
-             // comment line here
+            // comment line here
             var3 : UInt,
-             /* comment line there before 2 white space line above*/
+            /* comment line there before 2 white space line above*/
             func4 : Dynamic,
-             /* comment line there before white space*/
+            /* comment line there before white space*/
             var5 : Array<Dynamic>) : Bool
     {
         return true;
@@ -272,6 +333,11 @@ class @:final Main extends MyClass
 
     public function new()
     {
+        mIntKeyMap = null;
+        mStringKeyMap = null;
+        mObjectKeyMap = null;
+        mMapOfArray = null;
+        mMapOfMap = null;
         isResult1 = true;
         isResult2 = true;
         isResult3 = true;
