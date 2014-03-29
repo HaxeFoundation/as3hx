68385256cac8884251dacd281c2c4c3fef3340dd
Yannick <yannick.dominguez@gmail.com>
2014-03-29 17:30:26 +0100

    fixed for loop and typedef gen bugs

--- as3hx/Parser.hx
+++ as3hx/Parser.hx
@@ -1160,7 +1160,7 @@ class Parser {
     */
     function generateTypeIfNeeded(classVar : ClassField) : Void
     {
-        //this only applies to satic field attributes, as
+        //this only applies to static field attributes, as
         //they only define constants values
         if (!Lambda.has(classVar.kwds, "static")) {
             return;
@@ -1183,6 +1183,9 @@ class Parser {
             default:
                 return;
         }
+
+        if (expr == null)
+            return;
 
         //only applies if the array is initialised at
         //declaration
