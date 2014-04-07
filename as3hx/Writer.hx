c29d1cad9c64b73828c980566bdfe3594d661fb6
cj <cj@imcj.me>
2014-04-07 16:31:55 +0800

    merge imports defined type of first class.

--- as3hx/Writer.hx
+++ as3hx/Writer.hx
@@ -183,9 +183,6 @@ class Writer
 
     function writeImport(i : Array<String>)
     {
-        if (i[0] == "flash") {
-            i[0] = "nme";
-        }
         var type = properCaseA(i,true).join(".");
         if (!Lambda.has(this.imported, type)) { //prevent duplicate import
             write("import " + type + ";");
@@ -197,13 +194,13 @@ class Writer
     }
         
     function writeAdditionalImports(defPackage : Array<String>, allTypes : Array<Dynamic>,
-                    definedTypes : Array<Dynamic>)
+                    definedTypes : Array<String>)
     {
         // We don't want to import any type that is defined within
         // this file, so add each of those to the type import map
         // first.
         for(d in definedTypes) {
-        typeImportMap.set(d.name, null);
+        typeImportMap.set(d, null);
         }
 
         // Now convert each seen type enum into the corresponding
@@ -3105,13 +3102,30 @@ class Writer
         //as only one instance of Writer write all the files
         this.imported = [];
 
+        var defined:Array<String> = [];
+
+        for (type in program.typesDefd)
+            defined.push(type.name);
+
+        switch(program.defs[0]) {
+            case CDef(c):
+                for (meta in c.meta) {
+                    switch (meta) {
+                    case EImport(v):
+                        defined.push(v[v.length-1]);
+                    default:
+                    }
+                }
+            default:
+        }
+
         this.o = writer;
         this.genTypes = program.genTypes;
         this.pack = program.pack;
         writeComments(program.header);
         writePackage(program.pack);
         writeImports(program.imports);
-        writeAdditionalImports(program.pack, program.typesSeen, program.typesDefd);
+        writeAdditionalImports(program.pack, program.typesSeen, defined);
         writeGeneratedTypes(program.genTypes);
         writeDefinitions(program.defs);
         writeComments(program.footer);
