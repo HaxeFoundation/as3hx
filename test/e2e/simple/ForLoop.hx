050b18ad7e0cb4257a44b1d98a8978524dec2a50
Yannick <yannick.dominguez@gmail.com>
2014-05-11 14:12:55 +0200

    add new simple e2e tests

---
+++ test/e2e/simple/ForLoop.hx
@@ -0,0 +1,12 @@
+package test.package;
+
+class ForLoop
+{
+    public function new()
+    {
+        for (i in 0...10){
+
+        }
+    }
+}
+
---
+++ test/e2e/simple/Func.hx
@@ -0,0 +1,16 @@
+package test.package;
+
+class Min
+{
+    private var testAttr : Int = 1;
+
+    public function new()
+    {
+        var test : String = "hello";
+    }
+
+    private function testFun(){
+
+    }
+}
+
