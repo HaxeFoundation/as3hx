9075180ff2590f954c0a9c6e511d76123f401d90
SlavaRa <ratdg@ya.ru>
2016-08-04 12:59:00 +0400

    Fixes issue #69 (#74)

---
+++ test/issues/Issue69.as
@@ -0,0 +1,8 @@
+package {
+    public class Issue69 {
+        public function Issue69() {
+            var s:String = "";
+            var c:String = s.charAt();
+        }
+    }
+}
--- test/unit/as3hx/AS3HXTest.hx
+++ test/unit/as3hx/AS3HXTest.hx
@@ -34,6 +34,11 @@ class AS3HXTest {
         generate("Issue68.as", "Issue68_generated.hx", "Issue68.hx");
     }
     
+    @Test("string.charAt() -> string.charAt(0)")
+    public function issue69() {
+        generate("Issue69.as", "Issue69_generated.hx", "Issue69.hx");
+    }
+    
     function generate(as3FileName:String, generatedFileName:String, expectedFileName:String) {
         var issuesDirectory = FileSystem.absolutePath("test/issues");
         var content = File.getContent('$issuesDirectory/$as3FileName');
