4eb04492ce687f345df547fd2ce664f4f6c4a123
slavara <ratdg@ya.ru>
2016-08-04 16:53:31 +0300

    Fixes issue #28

--- test/unit/TestSuite.hx
+++ test/unit/TestSuite.hx
@@ -1,6 +1,7 @@
 import massive.munit.TestSuite;
 
 import as3hx.AS3HXTest;
+import as3hx.parsers.ExprParserTest;
 import as3hx.parsers.ImportParserTest;
 import as3hx.parsers.UseParserTest;
 import as3hx.ParserUtilsTest;
@@ -20,6 +21,7 @@ class TestSuite extends massive.munit.TestSuite
 		super();
 
 		add(as3hx.AS3HXTest);
+		add(as3hx.parsers.ExprParserTest);
 		add(as3hx.parsers.ImportParserTest);
 		add(as3hx.parsers.UseParserTest);
 		add(as3hx.ParserUtilsTest);
