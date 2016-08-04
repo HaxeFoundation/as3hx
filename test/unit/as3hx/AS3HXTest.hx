d10a9a71c0db45584b2b8df1e046fc44dee752bf
slavara <ratdg@ya.ru>
2016-08-04 18:23:31 +0300

    Cleanup

--- test/unit/as3hx/AS3HXTest.hx
+++ test/unit/as3hx/AS3HXTest.hx
@@ -75,6 +75,11 @@ class AS3HXTest {
     }
     
     @Test
+    public function issue71() {
+        generate("Issue71.as", "Issue71_generated.hx", "Issue71.hx");
+    }
+    
+    @Test
     public function issue81() {
         generate("Issue81.as", "Issue81_generated.hx", "Issue81.hx");
     }
@@ -102,11 +107,6 @@ class AS3HXTest {
     @Test("var b:Boolean = !int -> var b:Bool = int != 0")
     public function issue91() {
         generate("Issue91.as", "Issue91_generated.hx", "Issue91.hx");
-    }
-    
-    @Test
-    public function issue71() {
-        generate("Issue71.as", "Issue71_generated.hx", "Issue71.hx");
     }
     
     function generate(as3FileName:String, generatedFileName:String, expectedFileName:String) {
