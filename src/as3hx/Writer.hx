288fbee6446e652f543dfec0cb85f1d9cf9b12b4
SlavaRa <ratdg@ya.ru>
2016-08-04 20:45:27 +0300

    WIP

--- src/as3hx/Writer.hx
+++ src/as3hx/Writer.hx
@@ -1069,11 +1069,10 @@ class Writer
 
     function getModifiedIdent(s : String) {
         return switch(s) {
-            case "string":              "String";
             case "int":                 "Int";
             case "uint":                cfg.uintToInt ? "Int" : "UInt";
-            case "number","Number":     "Float";
-            case "boolean","Boolean":   "Bool";
+            case "Number": "Float";
+            case "Boolean": "Bool";
             case "Function":            cfg.functionToDynamic ? "Dynamic" : s;
             case "Object":              "Dynamic";
             case "undefined":           "null";
