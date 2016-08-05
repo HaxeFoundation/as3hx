fa6c1731e3cf2c14aa68a7e7637972a212a809cb
SlavaRa <ratdg@ya.ru>
2016-08-05 12:44:28 +0400

    refactor of tests for issues (#109)

--- test/unit/as3hx/AS3HXTest.hx
+++ test/unit/as3hx/AS3HXTest.hx
@@ -26,138 +26,140 @@ class AS3HXTest {
     
     @Test
     public function issue14() {
-        generate("Issue14.as", "Issue14_generated.hx", "Issue14.hx");
+        generate("Issue14.as", "Issue14.hx");
     }
     
     @Test("ternarny operator")
     public function issue28() {
-        generate("Issue28.as", "Issue28_generated.hx", "Issue28.hx");
+        generate("Issue28.as", "Issue28.hx");
     }
     
     @Test
     public function issue29() {
-        generate("Issue29.as", "Issue29_generated.hx", "Issue29.hx");
+        generate("Issue29.as", "Issue29.hx");
     }
     
     @Test("array.concat() -> array.copy()")
     public function issue32() {
-        generate("Issue32.as", "Issue32_generated.hx", "Issue32.hx");
+        generate("Issue32.as", "Issue32.hx");
     }
     
     @Test("string.charCodeAt() -> string.charCodeAt(0)")
     public function issue36() {
-        generate("Issue36.as", "Issue36_generated.hx", "Issue36.hx");
+        generate("Issue36.as", "Issue36.hx");
     }
     
     @Test("Xml(string) -> FastXML.parse(string)")
     public function issue37() {
-        generate("Issue37.as", "Issue37_generated.hx", "Issue37.hx");
+        generate("Issue37.as", "Issue37.hx");
     }
     
     @Test
     public function issue38() {
-        generate("Issue38.as", "Issue38_generated.hx", "Issue38.hx");
+        generate("Issue38.as", "Issue38.hx");
     }
     
     @Test("private var name = 'value'")
     public function issue52() {
-        generate("Issue52.as", "Issue52_generated.hx", "Issue52.hx");
+        generate("Issue52.as", "Issue52.hx");
     }
     
     @Test("array.length = 10 -> Compat.setArrayLegth(array, 10)")
     public function issue63() {
-        generate("Issue63.as", "Issue63_generated.hx", "Issue63.hx");
+        generate("Issue63.as", "Issue63.hx");
     }
     
     @Test("for (var i:int = 0, n:int = array.length; i < n; i++)")
     public function issue64() {
-        generate("Issue64.as", "Issue64_generated.hx", "Issue64.hx");
+        generate("Issue64.as", "Issue64.hx");
     }
     
     @Test
     public function issue65() {
-        generate("Issue65.as", "Issue65_generated.hx", "Issue65.hx");
+        generate("Issue65.as", "Issue65.hx");
     }
     
     @Test("array.slice() -> array.copy()")
     public function issue68() {
-        generate("Issue68.as", "Issue68_generated.hx", "Issue68.hx");
+        generate("Issue68.as", "Issue68.hx");
     }
     
     @Test("string.charAt() -> string.charAt(0)")
     public function issue69() {
-        generate("Issue69.as", "Issue69_generated.hx", "Issue69.hx");
+        generate("Issue69.as", "Issue69.hx");
     }
     
     @Test
     public function issue71() {
-        generate("Issue71.as", "Issue71_generated.hx", "Issue71.hx");
+        generate("Issue71.as", "Issue71.hx");
     }
     
     @Test
     public function issue81() {
-        generate("Issue81.as", "Issue81_generated.hx", "Issue81.hx");
+        generate("Issue81.as", "Issue81.hx");
     }
     
     @Test("JSON.parse(string) -> haxe.Json.parse(string)")
     public function issue83() {
-        generate("Issue83.as", "Issue83_generated.hx", "Issue83.hx");
+        generate("Issue83.as", "Issue83.hx");
     }
     
     @Test("uint(1) -> as3hx.Compat.parseInt(1)")
     public function issue85() {
-        generate("Issue85.as", "Issue85_generated.hx", "Issue85.hx");
+        generate("Issue85.as", "Issue85.hx");
     }
     
     @Test
     public function issue87() {
-        generate("Issue87.as", "Issue87_generated.hx", "Issue87.hx");
+        generate("Issue87.as", "Issue87.hx");
     }
     
     @Test("NaN -> Math.NaN")
     public function issue89() {
-        generate("Issue89.as", "Issue89_generated.hx", "Issue89.hx");
+        generate("Issue89.as", "Issue89.hx");
     }
     
     @Test("var b:Boolean = !int -> var b:Bool = int != 0")
     public function issue91() {
-        generate("Issue91.as", "Issue91_generated.hx", "Issue91.hx");
+        generate("Issue91.as", "Issue91.hx");
     }
     
     @Test("[].join() -> [].join(',')")
     public function issue93() {
-        generate("Issue93.as", "Issue93_generated.hx", "Issue93.hx");
+        generate("Issue93.as", "Issue93.hx");
     }
     
     @Test("array.push(1,2,3,4,5,6,7,8,9,0)")
     public function issue94() {
-        generate("Issue94.as", "Issue94_generated.hx", "Issue94.hx");
+        generate("Issue94.as", "Issue94.hx");
     }
     
     @Test
     public function issue95() {
-        generate("Issue95.as", "Issue95_generated.hx", "Issue95.hx");
+        generate("Issue95.as", "Issue95.hx");
     }
     
     @Test("s is String ? 1 : 0")
     public function issue96() {
-        generate("Issue96.as", "Issue96_generated.hx", "Issue96.hx");
+        generate("Issue96.as", "Issue96.hx");
     }
     
     @Test
     public function issue103() {
-        generate("Issue103.as", "Issue103_generated.hx", "Issue103.hx");
+        generate("Issue103.as", "Issue103.hx");
     }
     
-    function generate(as3FileName:String, generatedFileName:String, expectedFileName:String) {
+    function generate(as3FileName:String, expectedHaxeFileName:String) {
         var issuesDirectory = FileSystem.absolutePath("test/issues");
+        var generatedDirectoryPath = '$issuesDirectory/generated';
+        if (!FileSystem.exists(generatedDirectoryPath)) FileSystem.createDirectory(generatedDirectoryPath);
         var content = File.getContent('$issuesDirectory/$as3FileName');
         var program = parser.parseString(content, issuesDirectory, '$issuesDirectory/$as3FileName');
-        var fw = File.write('$issuesDirectory/$generatedFileName', false);
+        var fw = File.write('$generatedDirectoryPath/$expectedHaxeFileName', false);
         writer.process(program, fw);
         fw.close();
-        var expectedText = File.getContent('$issuesDirectory/$expectedFileName');
-        var actualText = File.getContent('$issuesDirectory/$generatedFileName');
+        var expectedText = File.getContent('$issuesDirectory/$expectedHaxeFileName');
+        var actualText = File.getContent('$generatedDirectoryPath/$expectedHaxeFileName');
         Assert.areEqual(expectedText, actualText);
     }
 }
