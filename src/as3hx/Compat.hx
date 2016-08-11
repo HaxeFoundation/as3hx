e2c171cb737bbe56c4efb36a7611afa7fb1b8cef
SlavaRa <ratdg@ya.ru>
2016-08-11 11:27:22 +0400

    Fix typo... (#153)

--- src/as3hx/Compat.hx
+++ src/as3hx/Compat.hx
@@ -38,7 +38,7 @@ class Compat {
         };
     }
 
-    public static inline function setArrayLegth<T>(a:Array<T>, length:Int) {
+    public static inline function setArrayLength<T>(a:Array<T>, length:Int) {
         if (a.length > length) a.splice(length, a.length - length);
         else a[length - 1] = null;
     }
