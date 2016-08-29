99692e0d79657b0e6fbc42f1fd93d675753ab019
slavara <ratdg@ya.ru>
2016-08-29 19:03:56 +0300

    Call `StringTools.trim(string)` instead of `mx.utils.StringUtil.trim(string)` Call `StringTools.isSpace(string, 0)` instead of `mx.utils.StringUtil.isWhitespace(string)`

--- src/as3hx/parsers/ImportParser.hx
+++ src/as3hx/parsers/ImportParser.hx
@@ -19,35 +19,41 @@ class ImportParser {
     public static function parse(tokenizer:Tokenizer, cfg:Config) {
         Debug.dbg("parseImport()", tokenizer.line);
         var a = [tokenizer.id()];
-        while( true ) {
+        while(true) {
             var tk = tokenizer.token();
-            switch( tk ) {
-            case TDot:
-                tk = tokenizer.token();
-                switch(tk) {
-                case TId(id):
-                    if (Lambda.has(doNotImport, id)) return [];
-                    // TODO: this is flash.utils.Proxy need to create a compat class
-                    // http://blog.int3ractive.com/2010/05/using-flash-proxy-class.html
-                    if (id == "flash_proxy") return ["flash","utils","Proxy"];
-                    // import __AS3__.vec.Vector;
-                    if (id == "Vector" && a[0] == "__AS3__") return [];
-                    // import flash.utils.Dictionary;
-                    if (cfg.dictionaryToHash && id == "Dictionary" && a.length == 2 && a[0] == "flash" && a[1] == "utils") return [];
-                    a.push(id);
-                case TOp(op):
-                    if(op == "*") {
-                        a.push(op);
-                        break;
+            switch(tk) {
+                case TDot:
+                    tk = tokenizer.token();
+                    switch(tk) {
+                        case TId(id):
+                            if (Lambda.has(doNotImport, id)) return [];
+                            // TODO: this is flash.utils.Proxy need to create a compat class
+                            // http://blog.int3ractive.com/2010/05/using-flash-proxy-class.html
+                            if (id == "flash_proxy") return ["flash","utils","Proxy"];
+                            // import __AS3__.vec.Vector;
+                            if (id == "Vector" && a[0] == "__AS3__") return [];
+                            // import flash.utils.Dictionary;
+                            if (cfg.dictionaryToHash && id == "Dictionary" && a.length == 2 && a[0] == "flash" && a[1] == "utils") return [];
+                            if (id == "StringUtil") {
+                                switch(a) {
+                                    case ["mx", "utils"]: return [];
+                                    default:
+                                }
+                            }
+                            a.push(id);
+                        case TOp(op):
+                            if(op == "*") {
+                                a.push(op);
+                                break;
+                            }
+                            ParserUtils.unexpected(tk);
+                        default: ParserUtils.unexpected(tk);
                     }
-                    ParserUtils.unexpected(tk);
-                default: ParserUtils.unexpected(tk);
-                }
-            case TCommented(s,b,t):
-                tokenizer.add(t);
-            default:
-                tokenizer.add(tk);
-                break;
+                case TCommented(s,b,t):
+                    tokenizer.add(t);
+                default:
+                    tokenizer.add(tk);
+                    break;
             }
         }
         Debug.dbgln(" -> " + a, tokenizer.line);
