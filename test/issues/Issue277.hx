79b3e62e2578a92bb0c4b077c8ccf683c462ba93
Markus Stefanko <markus.stefanko@gmail.com>
2017-10-20 19:44:42 +0700

    Fixed conversion of `else if` blocks. fixes #277
    * Fixes #277 - keeps `else if` statements together
    
    * Fixes #264 - adds comments before `else` blocks correctly
    
    * test for Issue264
    
    * test for Issue277
    
    * Updated CHANGES.md and formatting fixes

---
+++ test/issues/Issue277.hx
@@ -0,0 +1,51 @@
+
+class Issue277
+{
+    public function new()
+    {
+        if (1)
+        {
+            a();
+        }
+        else if (2)
+        {
+            b();
+        }
+        else if (3)
+        {
+            c();
+        }
+        else if (4)
+        {
+            d();
+        }
+        else if (5)
+        {
+            e();
+        }
+        else if (6)
+        {
+            f();
+        }
+        else if (7)
+        {
+            g();
+        }
+        else if (8)
+        {
+            h();
+        }
+        else if (9)
+        {
+            i();
+        }
+        else if (10)
+        {
+            j();
+        }
+        else
+        {
+            k();
+        }
+    }
+}
