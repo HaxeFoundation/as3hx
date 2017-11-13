6a5f51451dacb74c4a8664ea57c52f7b9ee1fe4b
Slava Buynov <slavab@playtika.com>
2017-11-13 21:35:43 +0300

    Copied test\issues\Issue261_1.hx to test\issues\Issue261_2.hx

---
+++ test/issues/Issue261_2.hx
@@ -0,0 +1,13 @@
+
+class Issue2611
+{
+    public function randomGen(param1 : Int) : Int
+    {
+        var _loc2_ : Int = Math.floor(Math.random() * param1);
+        return _loc2_;
+    }
+
+    public function new()
+    {
+    }
+}
